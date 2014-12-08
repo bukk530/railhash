e2b78ba1a0830a0eb8d8198bb7816ceaea105a63
bukk530 <bukk530@notebook>
2014-12-08 20:00:16 +0100

    Update

--- lib/railhash.rb
+++ lib/railhash.rb
@@ -3,7 +3,7 @@ require 'digest'
 module RailHash
   
   #Digest data using sha512
-  def get_hash(data, salted=true)
+  def self.get_hash(data, salted=true)
     if salted
       salt = Digest::SHA512.hexdigest( rand().to_s + Time.now.to_f.to_s + Process.pid.to_s )
       return Digest::SHA512.hexdigest( data + ":" + salt )
@@ -13,7 +13,7 @@ module RailHash
   end
   
   #Verify if hash was generated from the data
-  def verify(data, hash, salted=true)
+  def self.verify(data, hash, salted=true)
     if salted
       digest.split! ":"
       return Digest::SHA512.hexdigest( data + ":" + digest[1] ) == digest[0]
