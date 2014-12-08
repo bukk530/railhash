413476909200a7491a25f44fbdc9533dac0989de
bukk530 <bukk530@notebook>
2014-12-08 19:25:46 +0100

    Added SHA512 hashing

--- lib/railhash.rb
+++ lib/railhash.rb
@@ -1,5 +1,25 @@
+require 'digest'
+
 module Railhash
   
+  #Digest data using sha512
+  def get_hash(data, salted=true)
+    if salted
+      salt = Digest::SHA512.hexdigest( rand().to_s + Time.now.to_f.to_s + Process.pid.to_s )
+      return Digest::SHA512.hexdigest( data + ":" + salt )
+    else
+      return Digest::SHA512.hexdigest( data )
+    end
+  end
   
+  #Verify if hash was generated from the data
+  def verify(data, hash, salted=true)
+    if salted
+      digest.split! ":"
+      return Digest::SHA512.hexdigest( data + ":" + digest[1] ) == digest[0]
+    else
+      return Digest::SHA512.hexdigest( data ) == hash
+    end
+  end
   
 end
